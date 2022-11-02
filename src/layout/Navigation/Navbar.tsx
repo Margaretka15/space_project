e63f7e8334e748b104231959573a99f48e66db5f
Poocinka15 <poociek15@gmail.com>
2022-11-02 13:52:00 +0100

    Add menu for mobiles.

--- src/layout/Navigation/Navbar.tsx
+++ src/layout/Navigation/Navbar.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faBars, faClose} from '@fortawesome/free-solid-svg-icons'
+
+import React, {useState} from "react";
 
 type Props = {
     children: React.ReactElement[] | React.ReactElement;
     type: string;
 }
 
-function Navbar({children, type} :Props) {
+function Navbar({children, type}: Props) {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleMenu = () => {
+        setIsExpanded(isExpanded => !isExpanded)
+    }
+
     return (
-        <nav className={type}>
-            {children}
-        </nav>);
+        <>
+            <button className="navbar__hamburger"
+                    onClick={() => {
+                        toggleMenu();
+                    }}>
+                <FontAwesomeIcon icon={isExpanded ? faClose : faBars}/>
+            </button>
+            <nav className={!isExpanded ? "navbar" : "navbar navbar--expanded"}>
+                {children}
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
